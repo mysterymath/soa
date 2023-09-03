9b55ded500e8fea955fc050c9de2f54a7df4cf2c
Daniel Thornburgh <mysterymath@gmail.com>
2023-09-03 12:19:53 -0700

    It's aliiiiiive!

--- main.cc
+++ main.cc
@@ -24,16 +24,21 @@ void updateBalls(struct Ball *balls, int ballCnt) {
 
 template <typename T, size_t N> struct ArrayTuple;
 
+template <size_t N, typename T> struct ArrayTuple<std::tuple<T>, N> {
+  using Type = std::tuple<T[N]>;
+};
 template <size_t N, typename T, typename... Rest>
 struct ArrayTuple<std::tuple<T, Rest...>, N> {
   using Type = decltype(std::tuple_cat(
       std::declval<std::tuple<T[N]>>(),
-      std::declval<ArrayTuple<std::tuple<Rest...>, N>::Type>()));
+      std::declval<typename ArrayTuple<std::tuple<Rest...>, N>::Type>()));
 };
 
 template <typename T, size_t N> class StructOfArrays {
   using TupleType = decltype(boost::pfr::structure_to_tuple(std::declval<T>()));
   using ArrayTupleType = typename ArrayTuple<TupleType, N>::Type;
+
+  ArrayTupleType Tuple;
 };
 
 int main(void) {
