5d58cd35852d461d37f45cd078e4af5b573d6453
Daniel Thornburgh <mysterymath@gmail.com>
2023-09-08 10:43:03 -0700

    Start working on test

--- main.cc
+++ main.cc
@@ -2,32 +2,6 @@
 #include <soa.h>
 #include <string.h>
 
-#if 0
-struct Ball {
-  int x;
-  int y;
-  int dx;
-  int dy;
-};
-
-#define SOA_TYPE Ball
-#define SOA_MEMBERS MEMBER(x) MEMBER(y) MEMBER(dx) MEMBER(dy)
-#include <soa-impl.inc>
-
-soa::Array<Ball, 10> balls = {Ball{.x = 10, .dx = 20}};
-
-void updateBall(uint8_t Idx) {
-  auto ball = balls[Idx];
-  ball.x += ball.dx;
-  ball.y += ball.dy;
-}
-
-void updateBalls() {
-  for (int i = 0; i < balls.size(); i++)
-    updateBall(i);
-}
-#endif
-
 struct InnerStruct {
   const char *char_ptr;
 };
