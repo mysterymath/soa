8d047adcb79989062f8fb9275749d53d8cd34852
Daniel Thornburgh <mysterymath@gmail.com>
2023-09-08 09:23:30 -0700

    Add documentation

--- soa.h
+++ soa.h
@@ -8,6 +8,45 @@
 
 namespace soa {
 
+/// An array implemented as a struct of arrays.
+///
+/// This data structure is logically similar to a C-style array, but each byte
+/// of the array's element type is represented as a seperate array of bytes. In
+/// other words, if a C array were represented as a multidimimensional array of
+/// bytes, uint8_t Bytes[ArrayIdx][ByteIdx], this data structure is its
+/// transpose, uint8_t Bytes[ByteIdx][ArrayIdx]. This can provide more efficient
+/// addressing on the 6502, since its 8-bit indexed addressing modes preclude
+/// automatic scaling needed to efficiently access traditional C arrays.
+///
+/// Because the representation of the elements is broken apart, only trivial
+/// types with standard layouts are supported (think C-style types).
+///
+/// This class loses its performance benefit, and indeed, may hurt performance,
+/// if accessed through a pointer. All uses of this object should be by
+/// explicitly naming the definition. This tends to imply that the definition
+/// is global, but it may also be used as a local variable in functions that
+/// the compiler can prove do not recurse.
+template <typename T, uint8_t N> class Array;
+
+/// Reference to an array element.
+///
+/// This proxy provides access to the contents of a specific array element. If
+/// the type is arithmetic or a pointer, it can be used in numeric expressions
+/// with the full range of operators, including the various assignment
+/// operators. If it is a struct, by default the struct type is opaque. It's
+/// members are not directly accessible, and the struct can only be read or
+/// written as a whole. A specialization can be generated to allow for member
+/// access using the soa-struct.inc header; see that header for details.
+///
+/// References should not be stored more than temporarily, and they should not
+/// be used as arguments to functions. This may cause them to acually take on
+/// their logical representation at runtime (an array of pointers, one per
+/// byte), which is typically worse than using a regular C-style array.
+template <typename T, typename Enable = void> struct Ref;
+
+/// Base class for references to array elements.
+///
+/// Implements the constant part of the interface.
 template <typename T> struct BaseConstRef {
   static_assert(!std::is_volatile_v<T>, "volatile types are not supported");
   static_assert(std::is_trivial_v<T>, "non-trivial types are unsupported");
@@ -16,6 +55,9 @@ template <typename T> struct BaseConstRef {
   static_assert(std::alignment_of_v<T> == 1, "aligned types are not supported");
 
 protected:
+  // References to array elements are represented as an array of pointers to
+  // each byte. This keeps the representation agnostic of the size of the
+  // original array.
   const uint8_t *BytePtrs[sizeof(T)];
 
 public:
@@ -27,6 +69,17 @@ public:
       BytePtrs[ByteIdx] = &ByteArrays[ByteIdx][Idx];
   }
 
+  template <uint8_t N>
+  [[clang::always_inline]] constexpr BaseConstRef(const BaseConstRef &Other) {
+#pragma unroll
+    for (uint8_t I = 0; I < sizeof(T); ++I)
+      BytePtrs[I] = Other.BytePtrs[I];
+  }
+
+  /// Return the value of the reference.
+  ///
+  /// This provides a more readable syntax than casting for contexts where the
+  /// implicit conversion to T doesn't trigger, e.g., in printf.
   [[clang::always_inline]] T get() const { return static_cast<T>(*this); }
 
   [[clang::always_inline]] constexpr operator T() const {
@@ -43,7 +96,9 @@ public:
   }
 };
 
+/// Base class for mutable references to array elements.
 template <typename T> struct BaseRef : public BaseConstRef<T> {
+  // Cast away the constness from the base class's byte pointers.
   [[clang::always_inline]] uint8_t **byte_ptrs() const {
     return const_cast<uint8_t **>(BaseConstRef<T>::BytePtrs);
   }
@@ -67,8 +122,6 @@ public:
     return BaseConstRef<T>::get()(std::forward(Args)...);
   }
 };
-
-template <typename T, typename Enable = void> struct Ref;
 
 template <typename T>
 struct Ref<T,
@@ -192,8 +245,6 @@ struct Ref<T, std::enable_if_t<std::is_const_v<T>>> : public BaseConstRef<T> {
                                          uint8_t Idx)
       : BaseConstRef<T>(ByteArrays, Idx) {}
 };
-
-template <typename T, uint8_t N> class Array;
 
 template <typename T, uint8_t N> class ArrayConstIterator {
   friend class Array<T, N>;
